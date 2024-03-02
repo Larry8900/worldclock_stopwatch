b2fbdc485eb6f3ed24aab7ec86d896b4bb1fc73d
larry8900 <quickexchange488@gmail.com>
2024-03-02 01:42:19 +0100

    updated

---
+++ index2.js
@@ -0,0 +1,18 @@
+
+// get elements from the HTML DOM
+// header
+let userTime = document.getElementById('user-time');
+let userLocation = document.getElementById('user-location');
+let userDate = document.getElementById('user-date')
+
+// search
+let userInput = document.getElementById('search-input');
+let searchButton = document.getElementById('search-btn');
+
+// country cards using jquery methods
+// since the country name and flag will be constant i'll be selecting the time and weather only
+
+let temp1 = document.getElementById('weather1')
+let temp2 = document.getElementById('weather2')
+let temp3 = document.getElementById('weather3')
+let temp4 = document.getElementById('weather4')
