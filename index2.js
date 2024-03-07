ffc2e3e774339b9b089a660a07cab205b054120b
larry8900 <quickexchange488@gmail.com>
2024-03-07 23:48:02 +0100

    update

--- index2.js
+++ index2.js
@@ -1,10 +1,23 @@
 
 // get elements from the HTML DOM
 // header
-let userTime = document.getElementById('user-time');
+let userTime = document.getElementById('user_time');
 let userLocation = document.getElementById('user-location');
 let userDate = document.getElementById('user-date')
 
+// Date methhod
+let date = new Date()
+let hours = date.getHours()
+let minutes = date.getMinutes()
+let seconds = date.getSeconds()
+userTime.textContent = `${hours}: ${minutes}: ${seconds}`;
+
+// to get the user's location 
+
+
+$.getJSON("https://ipgeolocation.abstractapi.com/v1/?api_key=1edc3bed50bd418181131985c368acb7&ip_address=155.93.102.10", function(data) {
+    console.log(data);
+})
 // search
 let userInput = document.getElementById('user-input');
 
@@ -13,15 +26,15 @@ let searchButton = document.getElementById('search-btn');
 // country cards using jquery methods
 // since the country name and flag will be constant i'll be selecting the time and weather only
 
-let temp1 = document.getElementById('weather1')
-let temp2 = document.getElementById('weather2')
-let temp3 = document.getElementById('weather3')
-let temp4 = document.getElementById('weather4')
+
 
 searchButton.addEventListener('click',getTemp);
 
+// weather Api returns a particular city/country weather details including timezone, date and temperature
+// countryCode api is used to fetch a country is20 alpha code which is used to return the country flag
 
 async function getTemp() {
+    $('.search-display').css('display', 'flex')
     const city = userInput.value
     const weatherUrl = `https://api.weatherapi.com/v1/current.json?key=e881d1cec5db41f4b88231239240103&q=${city}&aqi=no`
     const countryCodeUrl = `https://country-codes4.p.rapidapi.com/country-codes?name=${city}`;
@@ -31,27 +44,37 @@ async function getTemp() {
             'X-RapidAPI-Key': '70cfdefaabmshc725a3f74bdafc3p1fad03jsn42865df74020',
             'X-RapidAPI-Host': 'country-codes4.p.rapidapi.com'
         }
-    };
-
+    }
     console.log(city)
     try{
         const response = await fetch(weatherUrl)
-        const weatherData = await response.json();
+        const weatherData = await response.json()
 
 
-        const response2 = await fetch(countryCodeUrl, options);
-        const countryCodeData = await response2.json();
+        const response2 = await fetch(countryCodeUrl, options)
+        const countryCodeData = await response2.json()
 
-        console.log(weatherData,countryCodeData);
-        
-        
+        console.log(weatherData,countryCodeData)
+        console.log()
+
         if(countryCodeData.length == 0){
-            $('#search-image').attr('src', weatherData.current.condition.icon);
+            $('#search-image').attr('src', weatherData.current.condition.icon)
+            $('#city').text(weatherData.location.name)
+            $('#time').text(weatherData.location.localtime)
+            $('#temp').text(weatherData.current.temp_c)
         }
+        // else if(weatherData.includes('error') ){
+        //     alert(weatherData.error.message)
+        //     $('.search-display').css('display', 'none')
+
+        // }
         else {
 
             let alpha2Code = countryCodeData[0].alpha2Code;
              $('#search-image').attr('src', `http://www.geognos.com/api/en/countries/flag/${alpha2Code}.png`)
+             $('#city').text(countryCodeData[0].name)
+             $('#time').text(weatherData.location.localtime)
+             $('#temp').text(weatherData.current.temp_c)
              console.log(alpha2Code)
         }
        
@@ -60,24 +83,5 @@ async function getTemp() {
     catch(error) {
         console.log(error)
     }
-
-    
-   
-    
-    // try {
-    //     const response = await fetch(url2, options);
-    //     const result = await response.json();
-    //     if (result.length >0) {
-    //         const alpha2CodeValue = result[0].alpha2Code; // saving the alpha2code into a variable
-    //       // changing the image source attribute on DOM  with jQuery 
-    //         $('img').attr('src', `http://www.geognos.com/api/en/countries/flag/${alpha2CodeValue}.png`);
-    //         console.log(result)
-    //     }
-
-    // } catch (error) {
-    //     console.error(error);
-    // }
-
-
 
 }
